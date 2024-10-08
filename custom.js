ae5aa92a4c97e08589c762358a867642a2c9f879
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-08 13:50:48 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/vmffprtm1@main/siege.mp4";
+        document.getElementById('vd').load();
+    }
+});
